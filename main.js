beefaa8383c637980c521bd4e183b7b31ca3ce16
siruba-54 <ff-sasu8230@docomo.ne.jp>
2014-12-05 16:07:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 var milkcocoa = new MilkCocoa("https://io-ki3b5dnl0.mlkcca.com");
 var chatDataStore = milkcocoa.dataStore("chat");
-var textArea, board;
+var textArea, board,textName;
 window.onload = function(){
   textArea = document.getElementById("msg");
   board = document.getElementById("board");
+  textName = document.gefElementById("textName");
 }
 
 function clickEvent(){
   var text = textArea.value;
-  sendText(text);
+  sendText(text,textName.value);
 }
 
-function sendText(text){
-  chatDataStore.push({message : text},function(data){
+function sendText(text,nm){
+  chatDataStore.push({uname : nm,content : text},function(data){
     console.log("送信完了!");
     textArea.value = "";
   });
 }
 
 chatDataStore.on("push",function(data){
-  addText(data.value.message);
+  addText(data.value);
 });
 
 function addText(text){
   var msgDom = document.createElement("li");
-  msgDom.innerHTML = text;
+  msgDom.innerHTML = text.uname+":"+text.content;
   board.insertBefore(msgDom, board.firstChild);
 }
